8510bdbccbb3de5484647e5418e6183e9970aee1
Fábio Souza <fhssouza@gmail.com>
2023-05-01 20:51:18 -0300

    Inicio do projeto

---
+++ src/main/java/com/souzatech/clickdesp/ClickdespApiApplication.java
@@ -0,0 +1,13 @@
+package com.souzatech.clickdesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickdespApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickdespApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souzatech/clickdesp/ClickdespApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzatech.clickdesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickdespApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
